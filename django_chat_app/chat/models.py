d507cf8c5e75ac4602a489413b1b38dc596e7fa8
Werbung06 <Werbung06@skuvgele.local>
2024-04-17 17:14:37 +0200

    setup message class, register in admin.py is pending

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -1,3 +1,12 @@
+from datetime import date
+from django.conf import settings
 from django.db import models
+from django.forms import DateField
 
 # Create your models here.
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    created_at = models.DateField(default=date.today)
+    # chat
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
+    reciever = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reciever_message_set')
