2abc2f48d9edd49805ad70cf30ad0fd3811ca66b
Werbung06 <Werbung06@skuvgele.local>
2024-04-18 17:10:30 +0200

    make new table visible at admin dashboard

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -7,6 +7,6 @@ from django.forms import DateField
 class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
-    # chat
+    #chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     reciever = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reciever_message_set')
