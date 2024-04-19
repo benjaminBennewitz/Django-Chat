89ab044e82bf0634658206e3ce64d6520be3385d
Werbung06 <Werbung06@skuvgele.local>
2024-04-19 14:03:57 +0200

    send first message with terminal log

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        print("Recieved data: " + request.POST['txtmessage'])
     return render(request, 'chat/index.html', {'username':'Ben'})
